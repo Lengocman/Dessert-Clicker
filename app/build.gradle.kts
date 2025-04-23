42f256c25b2d5883cfbed08779e74b3a9b097f29
Lengocman <noir012553@gmail.com>
2025-04-23 10:02:38 +0700

    Upload toàn bộ code dự án

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.example.dessertclicker"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.dessertclicker"
+        minSdk = 24
+        //noinspection OldTargetApi
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.material3:material3")
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.ui:ui")
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.ui:ui-graphics")
+    //noinspection UseTomlInstead
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    //noinspection UseTomlInstead
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    //noinspection UseTomlInstead
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
